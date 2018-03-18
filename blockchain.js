ebaf5295feaa13565e44ce21ca523df8924198ca
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-03-18 15:18:51 +0100

    Add proof of work

--- blockchain.js
+++ blockchain.js
@@ -39,6 +39,22 @@ class Blockchain {
   lastBlock = () => {
     return [...this.chain].pop();
   };
+
+  validProof(lastProof, proof) {
+    const guessHash = crypto
+      .createHmac(process.env.HASH_TYPE, process.env.CRYPTO_SECRET)
+      .update(`${lastProof}${proof}`)
+      .digest("hex");
+    return guessHash.substr(0, 5) === process.env.RESOLUTION_HASH;
+  }
+
+  proofOfWork(lastProof) {
+    let proof = 0;
+    while (!this.validProof(lastProof, proof)) {
+      proof++;
+    }
+    return proof;
+  }
 }
 
 module.exports = Blockchain;

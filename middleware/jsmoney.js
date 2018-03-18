7213e9c7e14064098bd73cb5db47a14ffd929f97
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-03-18 20:30:41 +0100

    Add jsmoney logic

---
+++ middleware/jsmoney.js
@@ -0,0 +1,56 @@
+const Blockchain = require("./blockchain");
+const { validationResult } = require("express-validator/check");
+
+class JsMoney {
+  constructor() {
+    this.blockchain = new Blockchain();
+  }
+  getChain(req, res, next) {
+    req.responseValue = {
+      message: "Get Chain",
+      chain: this.blockchain.chain
+    };
+    return next();
+  }
+  mine(req, res, next) {
+    const lastBlock = this.blockchain.lastBlock();
+    const lastProof = lastBlock.proof;
+    const proof = this.blockchain.proofOfWork(lastProof);
+
+    // Create a new transaction with from 0 (this node) to our node (NODE_NAME) of 1 jsmoney
+    this.blockchain.newTransaction("0", process.env.NODE_NAME, 1);
+
+    // Forge the new Block by adding it to the chain
+    const previousHash = this.blockchain.hash(lastProof);
+    const newBlock = this.blockchain.newBlock(proof, previousHash);
+
+    const responseValue = Object.assign(
+      {
+        message: "New Block mined"
+      },
+      newBlock
+    );
+    req.responseValue = responseValue;
+    return next();
+  }
+
+  newTransaction(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.mapped() });
+    }
+    const trans = req.body;
+    const index = this.blockchain.newTransaction(
+      trans["sender"],
+      trans["recipient"],
+      trans["amount"]
+    );
+    const responseValue = {
+      message: `Transaction will be added to Block ${index}`
+    };
+    req.responseValue = responseValue;
+    return next();
+  }
+}
+
+module.exports = new JsMoney();
